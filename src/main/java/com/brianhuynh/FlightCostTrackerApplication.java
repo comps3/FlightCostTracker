40dfa095dab2dc20a4953f9de04e1f3e118b11f2
Brian Huynh <brianhuynh1@gmail.com>
2017-01-28 17:26:37 -0800

    Setup project template

---
+++ src/main/java/com/brianhuynh/FlightCostTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.brianhuynh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightCostTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightCostTrackerApplication.class, args);
+	}
+}
